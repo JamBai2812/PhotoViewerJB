d95598f80c5d518deb4da2922f934bfe2ab96e71
Jamie Baird <james.baird@softwire.com>
2020-02-24 16:47:32 +0000

    Added simple heading and created file for 'photo viewer' section

--- photoviewer/src/App.js
+++ photoviewer/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {PhotoViewer} from "./PhotoApp/PhotoViewer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <body className="body">
+            <h1>Photo Viewer</h1>
+            <PhotoViewer/>
+        </body>
+
+    );
 }
 
 export default App;
---
+++ photoviewer/src/PhotoApp/PhotoViewer.js
@@ -0,0 +1,12 @@
+﻿import React from "react";  // import React (to provide access to JSX)
+
+function PhotoViewer() {    // declare a new function called 'PhotoViewer'
+    return (
+        <div>               
+            <h2>Selected Photo</h2>
+            <img src="https://i.picsum.photos/id/466/200/200.jpg"/>
+        </div>
+    );
+}
+
+export {PhotoViewer};       // Export the function so that it can be imported in other files
